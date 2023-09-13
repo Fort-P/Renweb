1ae18caae5131b7a294481bbc9df139dc0e9c1d1
Fort-P <fortroberts2006@gmail.com>
2023-09-13 12:27:06 -0400

    Updated .gitignore, renweb.py
    added function for http POST to renweb.py

--- renweb.py
+++ renweb.py
@@ -1,20 +1,42 @@
+# Import libraries
 from bs4 import BeautifulSoup as bs
 import requests
 import json
 
+# Define functions
+def POST(url, data):
+    """
+    Tries 5 times to sends a POST request to {url} with the data of {data}
+    If every try fails, raise an exception
+    Returns a requests object containing the POST
+    """
+    for i in range(0,5):
+        r = session.post(url, data=data)
+        if not r.status_code == 200 and i < 4: # It failed, but we haven't tried 5 times yet
+            pass
+        elif not r.status_code == 200: # We have failed everytime, and we are on the last try
+            raise Exception(f"Failed to POST to {url} with code {r.status_code}")
+        else: # We are all good, and the data was POSTed
+            break
+        return r # Return the POST request for external processing
+
+# Declare a few variables
 HOMEPAGE = "https://familyportal.renweb.com/school/index"
 url = "https://accounts.renweb.com/Account/Login"
 session = requests.session()
 
+# Load config
 with open('config.json') as file:
     config = json.load(file)
 
-print("Logging in to Renweb...", end="")
+print("Logging in to Renweb...", end="") # Inform the user what we are doing
 
+# Get token to bypass antibot
 response = session.get(url)
 soup = bs(response.content, "html.parser")
 token = soup.find("input", {"name": "__RequestVerificationToken"})["value"]
 
+# Create the initial payload
 payload = {
     "District": config['District'],
     "Username": config['Username'],
@@ -22,12 +44,10 @@ payload = {
     "signIn": "LOG IN",
     "__RequestVerificationToken": token}
 
-response = session.post(response.url, data=payload)
-if not response.status_code == 200:
-    raise Exception(f"Failed to POST to {url} with code {response.status_code}")
+response = POST(response.url, payload)
 
-response = session.get(HOMEPAGE)
-
+# We have to submit another form with some verification values since our "browser" doesn't support JavaScript
+response = session.get(HOMEPAGE) # This actually redirrects us to another webpage for verification
 soup = bs(response.content, "html.parser")
 code = soup.find("input", {"name": "code"})["value"]
 scope = soup.find("input", {"name": "scope"})["value"]
@@ -35,15 +55,18 @@ state = soup.find("input", {"name": "state"})["value"]
 session_state = soup.find("input", {"name": "session_state"})["value"]
 iss = soup.find("input", {"name": "iss"})["value"]
 
+# Craft the second payload
 payload = {"code": code,
            "scope": scope,
            "state": state,
            "session_state": session_state,
            "iss": iss}
-url = "https://familyportal.renweb.com/signin-oidc"
-response = session.post(url, data=payload)
-if not response.status_code == 200:
-    raise Exception(f"Failed to POST to {url} with code {response.status_code}")
+
+# POST the new payload
+url = "https://familyportal.renweb.com/signin-oidc" # New form submision URL
+response = POST(url, payload)
+
+# Go to homepage to confirm login
 response = session.get("https://familyportal.renweb.com/school/index")
 if response.status_code == 200 and response.url == HOMEPAGE:
     print("Success!")
